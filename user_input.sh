10412685cbb3af8cfdea177fd2ee71cb97bd360c
Lokjf <revekara85@gmail.com>
2024-06-02 11:08:20 +0530

    Add files via upload

---
+++ user_input.sh
@@ -0,0 +1,9 @@
+:'
+Get User Input:
+
+‘read’ command is used to take input from user in bash. Create a file named ‘user_input.sh’ and add the following script for taking input from the user. Here, one string value will be taken from the user and display the value by combining other string value.
+'
+#!/bin/bash
+echo "Enter Your Name"
+read name
+echo "Welcome $name to LinuxHint"
